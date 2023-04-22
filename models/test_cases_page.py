8bda1e731a00561f084df353824ba84e885ea4cb
kundzios <kundzios@gmail.com>
2023-04-22 16:57:34 +0200

    Fixed class being collected by pytest

--- models/test_cases_page.py
+++ models/test_cases_page.py
@@ -2,6 +2,8 @@ from .common_page import CommonPage
 
 
 class TestCasesPage(CommonPage):
+    __test__ = False
+
     def __init__(self, page):
         super().__init__(page)
         self.url += "test_cases"
